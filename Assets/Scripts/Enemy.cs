d931ee0641f25f633f7ecaf7bed9137a509d24fc
OnealTristan <tristanjulian37@gmail.com>
2024-01-24 15:19:21 +0700

    Update-Player_Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,36 +8,50 @@ public class Enemy : MonoBehaviour
 	public Action<float> OnDecreaseHPPlayer;
 
     [Header(" Skitterfang ")]
-    public bool skitterfangIsActive;
+    [SerializeField] private bool skitterfangIsActive;
     [SerializeField] private float skitterfangDamage;
     
     [Header(" Shadowfang ")]
-    public bool shadowfangIsActive;
+    [SerializeField] private bool shadowfangIsActive;
     [SerializeField] private float shadowfangDamage;
 
-	// Start is called before the first frame update
-	void Start()
-    {
-        
-    }
+	[Header(" Towerfang ")]
+	[SerializeField] private bool towerfangIsActive;
+	[SerializeField] private float towerfangDamage;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[Header(" Voidbane ")]
+	[SerializeField] private bool voidbaneIsActive;
+	[SerializeField] private float voidbaneDamage;
 
-    public void Skitterfang() {
+	public void ActivatedEnemy() {
         if (skitterfangIsActive == true) {
-            Debug.Log("Skitterfang Attack!");
-            OnDecreaseHPPlayer?.Invoke(skitterfangDamage);
+            Skitterfang();
+        } else if (shadowfangIsActive == true) {
+            Shadowfang();
+        } else if (towerfangIsActive == true) {
+            Towerfang();
+        } else if (voidbaneIsActive == true) {
+            Voidbane();
         }
     }
 
-    public void Shadowfang() {
-        if (shadowfangIsActive == true) {
-            Debug.Log("Shadowfang Attack!");
-            OnDecreaseHPPlayer?.Invoke(shadowfangDamage);
-        }
+    private void Skitterfang() {
+        Debug.Log("Skitterfang Attack!");
+        OnDecreaseHPPlayer?.Invoke(skitterfangDamage);
     }
+
+    private void Shadowfang() {
+        Debug.Log("Shadowfang Attack!");
+        OnDecreaseHPPlayer?.Invoke(shadowfangDamage);
+    }
+
+	private void Towerfang() {
+        Debug.Log("Towerfang Attack!");
+		OnDecreaseHPPlayer?.Invoke(towerfangDamage);
+	}
+
+	private void Voidbane() {
+		Debug.Log("Towerfang Attack!");
+		OnDecreaseHPPlayer?.Invoke(voidbaneDamage);
+	}
 }
