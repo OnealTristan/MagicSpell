c39cb230b2c9a72864c4939ed2714161f4d33e59
OnealTristan <tristanjulian37@gmail.com>
2024-02-21 15:57:27 +0700

    Tristan-Progress-MainMenuUI

--- Assets/Scripts/MainMenu/MainMenuScript.cs
+++ Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public void OnClickStartButton () {
+    private ScrollRect scrollRect;
+
+	private void Start() {
+		scrollRect = GetComponentInChildren<ScrollRect>();
+		scrollRect.onValueChanged.AddListener(ListenerMethod);
+	}
+
+	public void OnClickStartButton () {
         Debug.Log("Start Clicked");
         Loader.Load(Loader.Scene.LevelMenu);
     }
+
+	private void ListenerMethod(Vector2 value) {
+		Debug.Log("ListenerMethod: " + value);
+	}
 }
